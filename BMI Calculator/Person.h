757da8972ad987e0868b09ec82e989f7527cc9d2
andrezacm <andreza.cmedeiros@gmail.com>
2015-02-16 20:59:28 -0800

    Add Person class with weight and height properties

---
+++ BMI Calculator/Person.h
@@ -0,0 +1,15 @@
+//
+//  Person.h
+//  BMI Calculator
+//
+//  Created by Andreza da Costa Medeiros on 2/16/15.
+//  Copyright (c) 2015 Andreza da Costa Medeiros. All rights reserved.
+//
+
+@interface Person : NSObject
+
+  @property(retain, nonatomic) NSNumber * weight;
+  @property(retain, nonatomic) NSNumber * height;
+  @property(readonly)          NSNumber * bmi;
+
+@end
---
+++ BMI Calculator/Person.m
@@ -0,0 +1,17 @@
+//
+//  Person.m
+//  BMI Calculator
+//
+//  Created by Andreza da Costa Medeiros on 2/16/15.
+//  Copyright (c) 2015 Andreza da Costa Medeiros. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "Person.h"
+
+@implementation Person
+
+  @synthesize weight;
+  @synthesize height;
+
+@end
