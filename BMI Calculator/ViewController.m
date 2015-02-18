96569282d379342c4dd257b615c80ea5e6299965
andrezacm <andreza.cmedeiros@gmail.com>
2015-02-17 22:41:30 -0800

    Add email in header of files

--- BMI Calculator/ViewController.m
+++ BMI Calculator/ViewController.m
@@ -4,6 +4,7 @@
 //
 //  Created by Andreza da Costa Medeiros on 2/16/15.
 //  Copyright (c) 2015 Andreza da Costa Medeiros. All rights reserved.
+//  andreza.cmedeiros@gmail.com
 //
 
 #import "ViewController.h"
